c74c34f5fd836b470c4ba25b476d18bd18093cfb
Rabbi Ahmed Limon <rabbiahemed>
2022-11-10 22:38:51 +0600

    bmi

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Bmi from "./Bmi/Bmi";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <div className="bmi">
+        <Bmi></Bmi>
+      </div>
     </div>
   );
 };
